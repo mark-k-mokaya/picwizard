379e428067d0431e9c385a85153a2646a9c2e5f9
mark-k-mokaya <mark-k-mokaya.gmail.com>
2024-08-28 03:24:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'dist')));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(_dirname, 'dist', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 	});
 }
 
